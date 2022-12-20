a3c3789fd41eed584482a2f0976543306fed2a68
David Ramírez <rrdavid08@gmail.com>
2022-12-20 01:45:14 +0100

    Contador puntos añadido

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import random
 
 def opcion_piedra(player, bot):
+    global puntos_jugador
+    global puntos_maquina
     if player == "piedra" and bot == "tijeras":
+        puntos_jugador += 1
         return "¡Has ganado!"
     elif player == "piedra" and bot == "papel":
+        puntos_maquina += 1
         return "Has perdido..."
     elif player == "piedra" and bot == "piedra":
         return "Empate"
 
 def opcion_papel(player, bot):
+    global puntos_jugador
+    global puntos_maquina
     if player == "papel" and bot == "piedra":
+        puntos_jugador += 1
         return "¡Has ganado!"
     elif player == "papel" and bot == "tijeras":
+        puntos_maquina += 1
         return "Has perdido..."
     elif player == "papel" and bot == "papel":
         return "Empate"
 
 def opcion_tijeras(player, bot):
+    global puntos_jugador
+    global puntos_maquina
     if player == "tijeras" and bot == "papel":
+        puntos_jugador += 1
         return "¡Has ganado!"
     elif player == "tijeras" and bot == "piedra":
+        puntos_maquina += 1
         return "Has perdido..."
     elif player == "tijeras" and bot == "tijeras":
         return "Empate"
@@ -27,8 +39,8 @@ def opcion_tijeras(player, bot):
 
 print("Escribe 'piedra', 'papel' o 'tijeras' para empezar a jugar. Si quieres salir escribe 'salir': ")
 jugador = input("Elijo: ")
-puntos_jugador = 0
 puntos_maquina = 0
+puntos_jugador = 0
 
 
 while jugador != "salir":
@@ -43,3 +55,10 @@ while jugador != "salir":
         print(f"Tu rival elige: {maquina}")
         print(opcion_tijeras(jugador, maquina))
     jugador = input(f"Si quieres seguir jugando vuelve a probar: ")
+
+if puntos_jugador == puntos_maquina:
+    print("Habéis empatado")
+elif puntos_jugador < puntos_maquina:
+    print(f"Tienes {puntos_jugador} puntos y tu rival tiene {puntos_maquina} puntos \n Lo siento, has perdido")
+else:
+    print(f"Tienes {puntos_jugador} puntos y tu rival tiene {puntos_maquina} puntos \n ¡Enhorabuena, has ganado!")
