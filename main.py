f457b8f3bd773031cf6aa41d60db8aa816070ca2
David Ramírez <rrdavid08@gmail.com>
2022-12-20 22:43:14 +0100

    Comentarios del código añadidos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
-def opcion_piedra(player, bot):
+# FUNCION QUE CONTIENE LAS DISTINTAS OPCIONES SI EL USUARIO ELIGE PIEDRA
+def opcionPiedra(player, bot):
     global puntos_jugador
     global puntos_maquina
     if player == "piedra" and bot == "tijeras":
@@ -12,7 +13,8 @@ def opcion_piedra(player, bot):
     elif player == "piedra" and bot == "piedra":
         return "Empate"
 
-def opcion_papel(player, bot):
+# FUNCION QUE CONTIENE LAS DISTINTAS OPCIONES SI EL USUARIO ELIGE PAPEL
+def opcionPapel(player, bot):
     global puntos_jugador
     global puntos_maquina
     if player == "papel" and bot == "piedra":
@@ -24,7 +26,8 @@ def opcion_papel(player, bot):
     elif player == "papel" and bot == "papel":
         return "Empate"
 
-def opcion_tijeras(player, bot):
+# FUNCION QUE CONTIENE LAS DISTINTAS OPCIONES SI EL USUARIO ELIGE TIJERAS
+def opcionTijeras(player, bot):
     global puntos_jugador
     global puntos_maquina
     if player == "tijeras" and bot == "papel":
@@ -37,25 +40,30 @@ def opcion_tijeras(player, bot):
         return "Empate"
 
 
+
 print("Escribe 'piedra', 'papel' o 'tijeras' para empezar a jugar. Si quieres salir escribe 'salir': ")
 jugador = input("Elijo: ")
+
+# INICIALIZAMOS VARIABLES DE PUNTUACIÓN A 0
 puntos_maquina = 0
 puntos_jugador = 0
 
-
+# MENÚ DEL JUEGO EN EL QUE EL USUARIO ELIGE SI SALIR O SEGUIR JUGANDO
 while jugador != "salir":
     maquina = random.choice(["piedra", "papel", "tijeras"])
     if jugador == "piedra":
         print(f"Tu rival elige: {maquina}")
-        print(opcion_piedra(jugador, maquina))
+        print(opcionPiedra(jugador, maquina))
     elif jugador == "papel":
         print(f"Tu rival elige: {maquina}")
-        print(opcion_papel(jugador, maquina))
+        print(opcionPapel(jugador, maquina))
     elif jugador == "tijeras":
         print(f"Tu rival elige: {maquina}")
-        print(opcion_tijeras(jugador, maquina))
-    jugador = input(f"Si quieres seguir jugando vuelve a probar: ")
+        print(opcionTijeras(jugador, maquina))
+    jugador = input(f"Si quieres seguir jugando vuelve a probar, sinó, escribe 'salir': ")
 
+
+# RECUENTO DE PUNTOS Y RESULTADO DEL GANADOR
 if puntos_jugador == puntos_maquina:
     print("Habéis empatado")
 elif puntos_jugador < puntos_maquina:
